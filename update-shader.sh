32de4b9a2a5d69c8882e82f7563cb46b1a2ab5df
Shaun Williams <shaun.e.williams@gmail.com>
2017-06-06 21:47:46 -0500

    allow update-shader.sh to work for multiple shaders

--- update-shader.sh
+++ update-shader.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 # Dump the shader file into the appropriate Java string.
+function updateShader {
+  orig=$1
+  min=$2
+  java=$3
 
-orig=src/main/java/mod/flex.fs
-min=src/main/java/mod/flex.min.fs
-java=src/main/java/mod/render360/coretransform/render/Flex.java
-temp=temp
+  # compress shader to a single line
+  cat $orig | sed 's/$/\\n/' | tr -d '\r\n' | tr -d '\n' > $min
 
-search='private final String fragmentShader'
+  # find the line number in the java file to replace
+  search='private final String fragmentShader'
+  line=$(grep -n "$search" $java | cut -f1 -d:)
 
-cat $orig | sed 's/$/\\n/' | tr -d '\r\n' | tr -d '\n' > $min
+  # replace the shader line
+  cat \
+    <(head -n "$((line - 1))" $java) \
+    <(echo "  ${search} = \"$(cat $min)\";") \
+    <(tail -n "+$((line + 1))" $java) \
+    > temp
+  mv temp $java
 
-line=$(grep -n "$search" $java | cut -f1 -d:)
+  echo "Updated fragmentShader in $java"
+}
 
-cat \
-  <(head -n "$((line - 1))" $java) \
-  <(echo "  ${search} = \"$(cat $min)\";") \
-  <(tail -n "+$((line + 1))" $java) \
-  > $temp
-
-mv $temp $java
-
-echo "Updated fragmentShader in $java"
+# Update flex shader
+updateShader \
+  src/main/java/mod/flex.fs \
+  src/main/java/mod/flex.min.fs \
+  src/main/java/mod/render360/coretransform/render/Flex.java
